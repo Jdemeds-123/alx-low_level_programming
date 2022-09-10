4f29bd5c8a451076fe61327bd4f8b61bd63a42c1
Jdemeds-123 <osamson21@gmail.com>
2022-09-10 03:43:45 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always retuns 0
+ *
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (; a < 10; a++)
+	{
+		putchar('0' + a);
+	}
+	putchar('\n');
+	return (0);
+}
