966d1b4eabbf282da325c6ed0627430b1389b753
Jdemeds-123 <osamson21@gmail.com>
2022-09-15 11:54:40 +0300

    Keep doing hard things

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,31 +1,26 @@
 #include <stdio.h>
 /**
- * main - main function
- *
- * Return: nothing
+ * main - main block
+ * Description: computes and prints the sum of all the multiples of 3 or
+ * 5 below 1024 (excluded), followed by a new line
+ * Return: 0
  */
 int main(void)
 {
-	int counter = 2;
+	int i = 0;
+	unsigned long int a = 0, b = 1, next = 0;
 
-	float a = 1;
-	float b = a + 1;
-	float c = a + b;
-
-	printf("%.0f, ", a);
-	printf("%.0f, ", b);
-	while (counter < 98)
+	while (i < 98)
 	{
-		counter++;
-		printf("%.0f", c);
+		next = a + b;
 		a = b;
-		b = c;
-		c = a + b;
-		if (counter < 98)
-		{
+		b = next;
+		printf("%lu", next);
+
+		if (i < 97)
 			printf(", ");
-		}
+		i++;
 	}
-	printf("\n");
+	putchar('\n');
 	return (0);
 }
