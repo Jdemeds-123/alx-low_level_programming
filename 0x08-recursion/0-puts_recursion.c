de2f4f6dc97ae84dbaeff6ae497aa87f9e799fcc
Jdemeds-123 <osamson21@gmail.com>
2022-09-29 11:33:12 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,14 +2,15 @@
 /**
  * _puts_recursion - uses recursion
  * @s: string to recurse through
+ * Return: no return.
  */
 void _puts_recursion(char *s)
 {
-if (*s)
-{
-_putchar(*s);
-_puts_recursion(++s);
-}
-else
-_putchar('\n');
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
 }
